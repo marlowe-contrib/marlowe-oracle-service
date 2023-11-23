40d863d3377001054708787676dbf1ae047e7151
Ale Gadea <ale@filabs.dev>
2023-11-23 15:18:04 -0300

    Initial caffolding

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { Lucid } from "lucid-cardano"
+import { mkRestClient } from "@marlowe.io/runtime-rest-client";
+
+export async function main() {
+    const lucid = await Lucid.new();
+    // let runtimeURL = process.env.MARLOWE_RUNTIME_URL;
+    let runtimeURL = "https://marlowe-runtime-preprod-web.scdev.aws.iohkdev.io";
+
+    const client = mkRestClient(runtimeURL);
+    const hasValidRuntime = await client.healthcheck();
+
+    if (!hasValidRuntime) throw new Error("Invalid Marlowe Runtime instance");
+}
