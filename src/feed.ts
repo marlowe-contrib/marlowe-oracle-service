4c03cb8e56c9a7d23c901a0180e181fd93b673b1
Lola Aimar <lolaaimar14@gmail.com>
2023-11-28 20:38:34 -0300

    add currency type and finish draft of flow for a coingecko feed req

--- src/feed.ts
+++ src/feed.ts
@@ -1,12 +1,20 @@
 import fetch from "node-fetch";
 
-const CurrencyPair: { [key: string] : [string, string]; } = {
-    "ADAUSD": ["CARDANO", "USD"],
-    "USDADA": ["USD", "CAD"]
+type Currency = 'ADA' | 'USD'
+
+type CurrencyPair = {
+  from: Currency,
+  to: Currency
+}
+
+
+const KnownCurrencyPairs: { [key: string] : CurrencyPair; } = {
+    "Coingecko ADAUSD": { from: 'ADA', to: 'USD'},
+    "Coingeckp USDADA": { from: 'USD', to: 'ADA'},
 };
 
-async function queryFeed(c: string): Promise<any> {
-    const coingeckoApi = `https://api.coingecko.com/api/v3/simple/price?ids=${CurrencyPair[c][0]}&vs_currencies=${CurrencyPair[c][1]}`
+async function queryCoingecko(from: string, to: string): Promise<any> {
+    const coingeckoApi = `https://api.coingecko.com/api/v3/simple/price?ids=${from}&vs_currencies=${to}`
     try {
         const response = await fetch(coingeckoApi, {
           method: 'GET',
@@ -17,6 +25,8 @@ async function queryFeed(c: string): Promise<any> {
         if (!response.ok) {
           throw new Error(`Error! status: ${response.status}`);
         }
+        // Maybe add the parsing of this response here ?
+        // CG API returns :  { base: { quote: price } }
         const result = (await response.json());
         return result;
       } catch (error) {
@@ -25,4 +35,19 @@ async function queryFeed(c: string): Promise<any> {
       }
 }
 
-console.log(await queryFeed("ADAUSD"))
+function currencyToCoingecko (c : Currency) : string {
+   switch (c) {
+    case 'ADA' : return "CARDANO";
+    case 'USD' : return "USD";
+   }
+}
+
+export async function getCoingeckoPrice (cn : string) : Promise<any> {
+  const curPair = KnownCurrencyPairs[cn];
+  const from = currencyToCoingecko(curPair.from);
+  const to = currencyToCoingecko(curPair.to);
+  const result = await queryCoingecko(from, to);
+  console.log(result);
+}
+
+getCoingeckoPrice("Coingecko ADAUSD");
