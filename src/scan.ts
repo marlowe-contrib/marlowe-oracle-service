5f8b76e0d90bc63b54585211795e2967a807362e
Valentino Cerutti <valentinocerutti@gmail.com>
2023-12-04 18:15:06 -0300

    Feat #5: Updates scan module with next action

--- src/scan.ts
+++ src/scan.ts
@@ -1,43 +1,131 @@
-import { Option, none, isSome, toUndefined } from "fp-ts/Option"
-import { pipe } from 'fp-ts/function'
+import { RestClient } from "marlowe-runtime-rest-client-txpipe";
+import { ContractId, addressBech32 } from "@marlowe.io/runtime-core";
+import {
+    Address,
+    Bound,
+    ChoiceId,
+    ChoiceName,
+    mkEnvironment,
+    Next,
+    partyCmp,
+} from "marlowe-language-core-v1-txpipe";
 
-import { RestClient, mkRestClient } from "@marlowe.io/runtime-rest-client";
-import { AddressBech32, ContractId, addressBech32, contractId } from "@marlowe.io/runtime-core";
-import { Bound, ChoiceId, ChoiceName, Contract } from "@marlowe.io/language-core-v1";
-import { ContractHeader, ContractsRange, GetContractsRequest } from "@marlowe.io/runtime-rest-client/dist/esm/contract";
+import {
+    ContractHeader,
+    ContractsRange,
+    GetContractsRequest,
+} from "marlowe-runtime-rest-client-txpipe/dist/esm/contract";
+import { Option, isSome, none, toUndefined } from "fp-ts/lib/Option.js";
+import { pipe } from "fp-ts/lib/function.js";
+import { isRight, left, match, right } from "fp-ts/lib/Either.js";
+import { CanChoose } from "@marlowe.io/language-core-v1/dist/esm/next/applicables/canChoose";
 
+/**
+ * The OracleRequest type contains the necessary information to identify an
+ * IChoice action that needs to be resolved.
+ */
 export type OracleRequest = {
     contractId: ContractId;
     choiceId: ChoiceId;
-    choiceBounds: Bound;
+    choiceBounds: Bound[];
     validFrom: Date;
     validUntil: Date;
 };
 
-async function getAllContracts(client: RestClient, request: GetContractsRequest): Promise<ContractHeader[]> {
+/**
+ * Get all marlowe contracts, going through all pages.
+ *
+ * @param client The Marlowe-ts rest client to make the neccesary queries
+ * @param request The base request for the getContracts endpoint
+ * @returns A list containing all pages of responses
+ */
+async function getAllContracts(
+    client: RestClient,
+    request: GetContractsRequest
+): Promise<ContractHeader[]> {
     let allResponses: ContractHeader[] = [];
     let cursor: Option<ContractsRange> = none;
 
     do {
-        request.range = pipe(cursor, toUndefined)
-        const response = await client.getContracts(request)
-        allResponses.concat(response.headers)
-        cursor = response.nextRange
-    } while (isSome(cursor))
+        request.range = pipe(cursor, toUndefined);
+        const response = await client.getContracts(request);
+        allResponses = allResponses.concat(response.headers);
+        cursor = response.nextRange;
+    } while (isSome(cursor));
 
-    return allResponses
+    return allResponses;
 }
 
-export async function getActiveContracts(client: RestClient, address: AddressBech32, choices: [ChoiceName]): Promise<OracleRequest[]> {
+/**
+ * Look for active marlowe contracts that can have an IChoice action resolved by
+ * the MOS.
+ * Returns only choices that have the oracle address as owner and whose choice
+ * name is a valid choice name
+ *
+ * @param client The Marlowe-ts rest client to make the neccesary queries
+ * @param mosAddress The address of the MOS
+ * @param validChoiceNames Choice names that the MOS knows how to resolve
+ * @returns a list of request for the MOS to resolve
+ */
+export async function getActiveContracts(
+    client: RestClient,
+    mosAddress: Address,
+    validChoiceNames: [ChoiceName]
+): Promise<OracleRequest[]> {
+    const b32OracleAddr = addressBech32(mosAddress.address);
+
     const contractsRequest: GetContractsRequest = {
-        "partyAddresses": [address]
+        partyAddresses: [b32OracleAddr],
     };
 
-    const allResponses = await getAllContracts(client, contractsRequest)
+    const allResponses = await getAllContracts(client, contractsRequest);
 
-    const contractIds = allResponses.map( contract => {
-        return contract.contractId
-    })
+    const currentTime: Date = new Date();
+    const timeAfter5Minutes: Date = new Date(
+        currentTime.getTime() + 5 * 60 * 1000
+    );
 
-    return []
+    const promises = allResponses.map((contract) =>
+        client
+            .getNextStepsForContract(
+                contract.contractId,
+                mkEnvironment(currentTime)(timeAfter5Minutes),
+                []
+            )()
+            .then((nextAction) =>
+                pipe(
+                    nextAction,
+                    match(
+                        (_) => left("Error on next query"),
+                        (value: Next) => {
+                            const choices =
+                                value.applicable_inputs.choices.filter(
+                                    (elem: CanChoose) =>
+                                        validChoiceNames.includes(
+                                            elem.for_choice.choice_name
+                                        ) &&
+                                        partyCmp(
+                                            elem.for_choice.choice_owner,
+                                            mosAddress
+                                        ) === "EqualTo"
+                                );
+                            return !choices?.length
+                                ? left("Empty choices")
+                                : right({
+                                      contractId: contract.contractId,
+                                      choiceId: choices[0].for_choice,
+                                      choiceBounds:
+                                          choices[0].can_choose_between,
+                                      validFrom: currentTime,
+                                      validUntil: timeAfter5Minutes,
+                                  });
+                        }
+                    )
+                )
+            )
+    );
+
+    const allNextAction = await Promise.all(promises);
+
+    return allNextAction.filter(isRight).map((elem) => elem.right);
 }
