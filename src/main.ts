62f5ae8c61082374caa92fac6c24655bd2a108f8
Lola Aimar <lolaaimar14@gmail.com>
2023-12-12 16:50:27 -0300

    add infinite loop with delay

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import { Lucid, MaestroConfig } from 'lucid-cardano';
-import { ContractId } from '@marlowe.io/runtime-core';
 import { mkRestClient } from 'marlowe-runtime-rest-client-txpipe';
-import { ApplyInputsToContractRequest } from 'marlowe-runtime-rest-client-txpipe/dist/esm/contract/transaction/endpoints/collection';
 import { Address } from 'marlowe-language-core-v1-txpipe';
 
 import { parseMOSConfig, parseMOSEnv } from './config.ts';
 import { getActiveContracts } from './scan.ts';
 import { getApplyInputs } from './feed.ts';
-import { getTx } from './tx.ts';
+import { buildAndSubmit } from './tx.ts';
 
 export async function main() {
     const mosConfig = await parseMOSConfig();
@@ -20,13 +18,24 @@ export async function main() {
         address:
             'addr_test1vzuqvqzcnuy9pmrh2sy7tjucufmpwh8gzssz7v6scn0e04gxdvna9',
     };
-    const activeContracts = await getActiveContracts(
-        client,
-        mosAddress,
-        mosConfig.choiceNames
-    );
-    const applicableInputs = await getApplyInputs(mosAddress, activeContracts);
-    const tx = await getTx(mosEnv.signTxUrl, client, lucid, applicableInputs);
-    console.log(applicableInputs);
+
+    do {
+        const activeContracts = await getActiveContracts(
+            client,
+            mosAddress,
+            mosConfig.choiceNames
+            );
+
+        const applicableInputs = await getApplyInputs(mosAddress, activeContracts);
+        console.log(applicableInputs);
+        const txHash = await buildAndSubmit(
+            mosEnv.signTxUrl,
+            client,
+            lucid,
+            applicableInputs
+            );
+        console.log("TxHash: ", txHash);
+        await new Promise(r => setTimeout(r, mosConfig.delay));
+    } while (true)
 
 }
