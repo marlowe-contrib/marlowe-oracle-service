bec79cf38d7c1b46f36e65864849e48eeeff89d6
Lola Aimar <lolaaimar14@gmail.com>
2023-12-12 15:30:33 -0300

    remove loop

--- src/tx.ts
+++ src/tx.ts
@@ -3,7 +3,6 @@ import { RestClient } from 'marlowe-runtime-rest-client-txpipe';
 import { ApplyInputsToContractRequest } from 'marlowe-runtime-rest-client-txpipe/dist/esm/contract/transaction/endpoints/collection';
 import { Lucid } from 'lucid-cardano';
 import axios, { AxiosError } from 'axios';
-import { ContractId, TextEnvelope } from '@marlowe.io/runtime-core';
 
 export async function signTx(signURL: string, cborHex: string) {
     try {
@@ -29,24 +28,29 @@ export async function signTx(signURL: string, cborHex: string) {
     }
 }
 
-export async function getTx(signTxUrl: string, client: RestClient, lucid: Lucid, applicableInputs: ApplyInputsToContractRequest[])
-: Promise<string[]> {
-    const txHashes: string[] = [];
-    for (const input of applicableInputs) {
-        try {
-            const appliedInput = await client.applyInputsToContract(input);
-            const signedCbor = await signTx(signTxUrl, appliedInput.tx.cborHex);
-            const submitted = await lucid.provider.submitTx(signedCbor);
-            txHashes.push(submitted);
-        } catch (error) {
-            if (axios.isAxiosError(error)) {
-                const e = error as AxiosError;
-                console.error("Axios error occurred: " + e.response?.statusText.toString());
-                console.error(e.response?.data);
-            } else {
-                console.error("Unexpected error occured", error)
-            }
+export async function buildAndSubmit(
+    signTxUrl: string,
+    client: RestClient,
+    lucid: Lucid,
+    applicableInputs: ApplyInputsToContractRequest[]
+): Promise<string> {
+    try {
+        const appliedInput = await client.applyInputsToContract(
+            applicableInputs[0]
+        );
+        const signedCbor = await signTx(signTxUrl, appliedInput.tx.cborHex);
+        const txHash = await lucid.provider.submitTx(signedCbor);
+        return txHash;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            const e = error as AxiosError;
+            console.error(
+                'Axios error occurred: ' + e.response?.statusText.toString()
+            );
+            console.error(e.response?.data);
+        } else {
+            console.error('Unexpected error occurred', error);
         }
+        return 'Error occurred';
     }
-    return txHashes;
 }
