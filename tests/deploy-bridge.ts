c15b498f12c524549ed6865bedc4316fcbacd4f4
Lola Aimar <lolaaimar14@gmail.com>
2024-01-12 17:05:48 -0300

    change address to which the scriptRef is payed

--- tests/deploy-bridge.ts
+++ tests/deploy-bridge.ts
@@ -1,30 +1,48 @@
-import { Data, Lucid, Script, applyDoubleCborEncoding } from "lucid-cardano";
-import { parseMOSEnv } from "../src/config.ts";
+import {
+    Constr,
+    Data,
+    Lucid,
+    Script,
+    applyDoubleCborEncoding,
+} from 'lucid-cardano';
+import { parseMOSEnv } from '../src/config.ts';
 
 const mosEnv = parseMOSEnv();
 const lucid = await Lucid.new(mosEnv.provider, mosEnv.network);
 lucid.selectWalletFromPrivateKey(mosEnv.signingKey);
 
 const spendScript: Script = {
-  type: "PlutusV2",
-  script: "588a0100003232323232323232322223253330083371e6eb8cc014c01c0052000489055370656e6400149858c8010c94ccc020cdc3a400000226464a66601a601e0042930b1bae300d001300700416300837540066600200290001111199980319b8700100300a233330050053370000890011806000801001118021baa0015734aae7555cf2ab9f5742ae89"
+    type: 'PlutusV2',
+    script: '587c0100003232323232323232322223253330083253330093370e90000008a5114a0600e0022930b1900199299980419b874800000454ccc02cc028dd50018a4c2c2c600c0046600200290001111199980319b8700100300a233330050053370000890011806000801001118019baa0015734aae7555cf2ab9f5742ae89',
 };
 
-const alwaysTrue = "500100003222253330044a22930b2b9a01";
+const alwaysTrue = '500100003222253330044a22930b2b9a01';
 
 const spendAddress = lucid.utils.validatorToAddress(spendScript);
 
 const alwaysTrueScript: Script = {
-  type: "PlutusV2",
-  script: applyDoubleCborEncoding(alwaysTrue)
+    type: 'PlutusV2',
+    script: applyDoubleCborEncoding(alwaysTrue),
 };
 
+const scriptAddress = lucid.utils.validatorToAddress(alwaysTrueScript);
+console.log('Script Address: ', scriptAddress);
+
+const utxos = await lucid.utxosAt(
+    'addr_test1wpdsmr0zyuvdh6uw8mus9ey7ccuty7t52n8vuutlyqf3yvsq4dxyh'
+);
+const redeemer = Data.to(new Constr(0, []));
+
 const tx = await lucid
-  .newTx()
-  .payToContract(spendAddress, { inline: Data.void(), scriptRef: alwaysTrueScript }, {})
-  .complete();
+    .newTx()
+    .collectFrom(utxos, redeemer)
+    .payToContract(
+        spendAddress,
+        { inline: Data.void(), scriptRef: alwaysTrueScript },
+        {}
+    )
+    .attachSpendingValidator(spendScript)
+    .complete();
 const txSigned = await tx.sign().complete();
-// console.log(txSigned.toString())
 const txHash = await txSigned.submit();
 console.log(`Transaction submitted. TxHash: ${txHash}`);
-
